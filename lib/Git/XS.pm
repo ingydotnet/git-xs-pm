39169377037aecab381a9ed2e6ebd2aa30df1b45
Ingy döt Net <ingy@ingy.net>
2011-12-25 21:07:44 -0800

    First commit

---
+++ lib/Git/XS.pm
@@ -0,0 +1,24 @@
+##
+# name:      Git::XS
+# abstract:  Perl XS Wrapper of libgit2
+# author:    Ingy döt Net <ingy@cpan.org>
+# license:   perl
+# copyright: 2011
+
+use 5.010;
+use strict;
+use warnings;
+
+package Git::XS;
+
+our $VERSION = '0.10';
+
+1;
+
+=head1 SYNOPSIS
+
+...
+
+=head1 DESCRIPTION
+
+...
